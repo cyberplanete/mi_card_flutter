8a55e573766d2b06821e7ae6139379ef082e20fe
Pascal <pascal.joret@gmail.com>
2020-09-30 14:27:25 +0200

    margin - padding - container in widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.red,
+        //A widget that insets its child by sufficient padding to avoid intrusions by the operating system.
+        body: SafeArea(
+          child: Container(
+            child: Center(child: Text('Hello')),
+            height: 100,
+            width: 100,
+            color: Colors.white,
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.fromLTRB(10, 15, 20, 5),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
